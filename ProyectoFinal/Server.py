9309dc6d496442b7e84b1b4316bc174ec638d0d3
PameNaFe <pnaranjoferrara@gmail.com>
2024-06-29 00:44:05 -0600

    Actualizacion servidor

--- ProyectoFinal/Server.py
+++ ProyectoFinal/Server.py
@@ -22,36 +22,6 @@ class Server(tk.Frame):
         self.t3.start()
         timesl.sleep(1)
 
-        #Thread to listen client requests
-        #self.t1 = threading.Thread(target=self.reciveRequests)
-        #self.t1.start()
-
-        #Thread to adjust clock
-        #self.t2 = threading.Thread(target=self.adjustHour)
-        #self.t2.start()
-
-        #Thread to refresh the interface
-        #self.t4 = threading.Thread(target=self.refreshInterface)
-        #self.t4.start()
-
-    def reciveRequests(self):
-        self.startNewSession()
-        receiveSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        receiveSocket.bind((self.host_ip, 7900))
-
-        while True:
-            clientSock = receiveSocket.recvfrom(1024)
-            if clientSock[0]:
-                #Send hour to client
-                hour = self.clock.returnHourTime()
-                receiveSocket.sendto(pickle.dumps(hour), clientSock[1])
-                #DB Queries
-                userExists = self.bookDB.userExists(clientSock[1])
-                if userExists != None:
-                    self.syncUploadDB(userExists)
-                self.requestBook(clientSock[1], hour) 
-                #Send book to client
-                receiveSocket.sendto(self.bookName.encode(), clientSock[1])
 
     #ejecuta hilo
     def notifyTracker(self):
